f19f9388586e05ba785ced07392f5c1c7d2f9d20
Miłosz Fede <miloszfede@gmail>
2024-02-11 17:42:05 +0100

    added magic wand to wheel of fortune

--- inventory.py
+++ inventory.py
@@ -1,7 +1,7 @@
 import random
 def add_to_inventory():
     inventory = []
-    wheel_of_fortune = ['dagger', 'gold', 'knife', 'sword', 'armor' ]
+    wheel_of_fortune = ['dagger', 'gold', 'knife', 'sword', 'armor', 'magic wand' ]
     won_item = random.choice(wheel_of_fortune)
     inventory.append(won_item)
     return print(won_item)
