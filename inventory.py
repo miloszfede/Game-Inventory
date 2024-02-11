73395b32e60afa357346b57dd3ec29c216a8d0ef
Miłosz Fede <miloszfede@gmail>
2024-02-11 17:35:08 +0100

    def add to inventory

--- inventory.py
+++ inventory.py
@@ -1,2 +1,9 @@
-def game_inventory():
-    print(2)
+import random
+def add_to_inventory():
+    inventory = []
+    wheel_of_fortune = ['dagger', 'gold', 'knife', 'sword', 'armor' ]
+    won_item = random.choice(wheel_of_fortune)
+    inventory.append(won_item)
+    return print(won_item)
+
+add_to_inventory()
